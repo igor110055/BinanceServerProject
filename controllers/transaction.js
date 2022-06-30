c9cc97cb9250fd688b7749e1fb09f0f238e9d22f
Nightfuury <rupsri1996@gmail.com>
2022-07-01 00:54:41 +0530

    pagination setup

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,8 +1,16 @@
 var Transaction = require('../Schema/transaction.schema');
 
+const per_page = 10;
+
 const getAllTransactions = async (req, res) => {
+    let page = req.query.page;
+
     const trxns = await Transaction.Transaction.find()
-	res.send(trxns);
+        .limit(per_page * 1)
+        .skip((page - 1) * per_page)
+        .exec();
+
+    res.send(trxns);
 }
 
 module.exports = {
